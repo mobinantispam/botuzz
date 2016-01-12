3b3a345309c522a5613c6f0b473f0dc3777ea9ad
mobinantispam <m.fdostiha@gmail.com>
2016-01-12 04:48:03 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {134177004},
+    sudo_users = {134177004,85831686},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
